af18c1efa9fe627a7864ed434acd1430f131d701
veda-sri-varsha <vedasrivarsha.kura@gmail.com>
2025-07-26 08:13:12 +0000

    Add to find the maximum of three numbers

---
+++ If-Else/02_MaxOfThreeNumbers.js
@@ -0,0 +1,11 @@
+function maxOfThree(a, b, c) {
+  if (a >= b && a >= c) {
+    return a;
+  } else if (b >= a && b >= c) {
+    return b;
+  } else {
+    return c;
+  }
+}
+
+console.log(maxOfThree(100, 200, 30)); // 200
