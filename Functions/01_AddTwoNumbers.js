5d2445c9337e6a15ae5285c5e7ec66d8a9de16ce
veda-sri-varsha <vedasrivarsha.kura@gmail.com>
2025-07-27 07:59:47 +0000

    Add function to sum two numbers

---
+++ Functions/01_AddTwoNumbers.js
@@ -0,0 +1,11 @@
+// 1. Add Two Numbers
+//  Create a function named `add(a, b)` that takes two numeric arguments and returns their sum.
+//  Example:
+//   add(5, 3) => 8
+
+function add(a,b){
+    return a+b;
+}
+
+console.log(add(5,3)); // Output: 8
+console.log(add(10, 20)); // Output: 30
